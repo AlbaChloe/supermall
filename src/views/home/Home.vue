fe0d4319eecfe4fc0dd5289ede76839ce24c8d3f
Chloe <951286894@qq.com>
2021-07-11 21:11:44 +0800

    second commit

---
+++ src/views/home/Home.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="home">
+    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    <home-swiper :banners="banners"></home-swiper>
+    <recommend-view :recommends="recommends"></recommend-view>
+    <feature-view></feature-view>
+  </div>
+</template>
+
+<script>
+import NavBar from "components/common/navbar/NavBar.vue";
+import HomeSwiper from "./childComps/HomeSwiper.vue";
+import RecommendView from "./childComps/RecommendView.vue";
+import FeatureView from "./childComps/FeatureView.vue";
+
+import { getHomeMultidata } from "network/home";
+
+export default {
+  name: "Home",
+  components: {
+    NavBar,
+    HomeSwiper,
+    RecommendView,
+    FeatureView,
+  },
+  data() {
+    return {
+      banners: [],
+      recommends: [],
+    };
+  },
+  created() {
+    // 1. 请求多个数据
+    getHomeMultidata().then((res) => {
+      this.banners = res.data.banner.list;
+      this.recommends = res.data.recommend.list;
+      console.log(res);
+    });
+  },
+};
+</script>
+
+<style scoped>
+.home-nav {
+  background-color: var(--color-tint);
+  color: white;
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  /* 导航栏除了固定定位之外的方法二： */
+  /* position: sticky; */
+  /* top: 0; */
+  z-index: 2;
+}
+
+#home {
+  padding-top: 44px;
+}
+
+img {
+  width: 100%;
+}
+</style>
---
+++ vue.config.js
@@ -0,0 +1,14 @@
+
+module.exports = {
+  configureWebpack: {
+    resolve: {
+      alias: {
+        'assets': '@/assets',
+        'common': '@/common',
+        'views': '@/views',
+        'components': '@/components',
+        'network': '@/network',
+      }
+    }
+  }
+}
